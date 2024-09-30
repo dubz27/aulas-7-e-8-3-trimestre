e4f23f9449c0458be0831228e9e4d8b123efb733
jvrazera <133227780+dubz27@users.noreply.github.com>
2024-09-30 07:46:26 -0300

    Add files via upload

---
+++ aulas-4-5-e-6-terceiro-tri-main/flashcard-aula_6/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'artes',
+    'o que seria o estilo de pintura do claude monet?',
+    'seria uma pintura a base de tinta a oleo'
+)
+
+criaCartao(
+    'artes',
+    'quantos quadros o van gogh vendeu em sua vida?',
+    'apenas um em toda sua vida apesar de ter feito mais de 400 quadros em toda sua vida'
+)
+
+criaCartao(
+    'artes',
+    'por que a coloração da obras de  arte do van gogh são tão puxadas para o amarelo?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
